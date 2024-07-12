dc88dc6dc45379079e27b4266ff66eaa7583d6b3
Abdul Samad <50093236+abdulsamad100@users.noreply.github.com>
2024-07-12 16:27:11 +0500

    add Question Panel

--- admin.js
+++ admin.js
@@ -1,3 +1,11 @@
 function addQuestion(){
     
 }
+function showAdd(){
+    document.querySelector("#default").classList.add("hideit")
+    document.querySelector("#addPanel").classList.remove("hideit");
+}
+function showDefault(){
+    document.querySelector("#default").classList.remove("hideit")
+    document.querySelector("#addPanel").classList.add("hideit");
+}
