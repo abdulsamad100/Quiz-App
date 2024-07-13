926b1b590a19ca9e907fe30b26d145b99bebce12
Abdul Samad <samadjs100@gmail.com>
2024-07-13 10:59:05 +0500

    Delete and Add Question Completed

--- admin.js
+++ admin.js
@@ -1,11 +1,125 @@
-function addQuestion(){
-    
+function getSelectedRadioValue() {
+    const radios = document.getElementsByName('ans');
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            return radios[i].id;
+        }
+    }
+    return null;
 }
-function showAdd(){
-    document.querySelector("#default").classList.add("hideit")
+
+function addQuestion() {
+    const qs = document.querySelector("#qstoadd").value;
+    const op1 = document.querySelector("#op1").value;
+    const op2 = document.querySelector("#op2").value;
+    const op3 = document.querySelector("#op3").value;
+    const op4 = document.querySelector("#op4").value;
+    const ansradio = getSelectedRadioValue();
+    let ans = null;
+
+    switch (ansradio) {
+        case "ans1":
+            ans = op1;
+            break;
+        case "ans2":
+            ans = op2;
+            break;
+        case "ans3":
+            ans = op3;
+            break;
+        case "ans4":
+            ans = op4;
+            break;
+    }
+
+    if (!qs || !op1 || !op2 || !op3 || !op4) {
+        alert("Kindly Fill all Fields");
+    } else if (!ans) {
+        alert("Kindly Select the correct answer");
+    } else {
+        const QuizQuestions = JSON.parse(localStorage.getItem('Quiz-Questions')) || [];
+
+        // Check if the question already exists
+        const duplicate = QuizQuestions.find(q => q.question === qs);
+
+        if (duplicate) {
+            alert("Question already exists");
+        }
+        else {
+            const qstoUpload = { question: qs, qop1: op1, qop2: op2, qop3: op3, qop4: op4, ans: ans };
+            QuizQuestions.push(qstoUpload); // Add new question
+            localStorage.setItem('Quiz-Questions', JSON.stringify(QuizQuestions)); // Save updated array
+            alert("Question added");
+
+            document.querySelector("#qstoadd").value = "";
+            document.querySelector("#op1").value = "";
+            document.querySelector("#op2").value = "";
+            document.querySelector("#op3").value = "";
+            document.querySelector("#op4").value = "";
+            const radios = document.getElementsByName('ans');
+            for (let i = 0; i < radios.length; i++) {
+                radios[i].checked = false;
+            }
+        }
+
+    }
+}
+
+function showAdd() {
+    document.querySelector("#default").classList.add("hideit");
     document.querySelector("#addPanel").classList.remove("hideit");
 }
-function showDefault(){
-    document.querySelector("#default").classList.remove("hideit")
+
+function showDefault() {
+    document.querySelector("#default").classList.remove("hideit");
     document.querySelector("#addPanel").classList.add("hideit");
+    document.querySelector("#dltPanel").classList.add("hideit");
+}
+
+function showDelete() {
+    const QuizQuestions = JSON.parse(localStorage.getItem('Quiz-Questions')) || [];
+    const divContent = document.querySelector("#dltPanel");
+
+    divContent.innerHTML = "<h1>Delete Question</h1>";
+
+    if (QuizQuestions.length > 0) {
+        QuizQuestions.forEach((question, index) => {
+            const questionNumber = index + 1;
+            const mainDiv = `
+        <div id="qpanel">
+            <span id="qnum">Question # ${questionNumber}</span>
+            <span id="question">${question.question}</span><br>
+            <label for="option1" id="qop1">Option 1: ${question.qop1}</label><br>
+            <label for="option2" id="qop2">Option 2: ${question.qop2}</label><br>
+            <label for="option3" id="qop3">Option 3: ${question.qop3}</label><br>
+            <label for="option4" id="qop4">Option 4: ${question.qop4}</label><br>
+            <button type="button" class="btn btn-danger" id="${questionNumber}" onclick="dltqs(${questionNumber - 1})">Delete</button><br>
+        </div>
+        `;
+            divContent.innerHTML += mainDiv;
+            // console.log("running");
+        });
+    } else {
+        divContent.innerHTML = `<span>No questions to delete</span><br>`;
+    }
+    divContent.innerHTML+=`<a onclick="showDefault()" class="pe-auto">Go Back</a>`
+    document.querySelector("#dltPanel").classList.remove("hideit");
+    document.querySelector("#default").classList.add("hideit");
+}
+
+function dltqs(num) {
+    const QuizQuestions = JSON.parse(localStorage.getItem('Quiz-Questions')) || [];
+
+    if (num >= 0 && num < QuizQuestions.length) {
+        QuizQuestions.splice(num, 1); // Remove the question at the specified index
+        localStorage.setItem('Quiz-Questions', JSON.stringify(QuizQuestions)); // Save the updated array back to localStorage
+        alert("Question deleted");
+        showDelete();
+    } else {
+        alert("Invalid question number");
+    }
+}
+
+function adminLogout(){
+    window.location.href = 'index.html';
 }
--- questions.js
+++ questions.js
@@ -1,21 +1,3 @@
-var question = [{
-    question: "Your Name:",
-    qop1: "Samad",
-    qop2: "Ahmed",
-    qop3: "Yousuf",
-    qop4: "Ali",
-    ans: "Samad"
-},
-{
-    question: "Your Father Name:",
-    qop1: "Abdullah",
-    qop2: "Ali",
-    qop3: "Mehmood",
-    qop4: "Hassan",
-    ans: "Mehmood"
-}];
-
-var qlength = question.length;
 var correctans = 0;
 var qstoexecute = 0;
 
@@ -27,101 +9,105 @@ function shuffle(array) {
     return array;
 }
 
-// localStorage.removeItem('shuffledQuestions')
-if (!localStorage.getItem('shuffledQuestions')) {
-    var shuffledQuestions = shuffle([...question]);
-    localStorage.setItem('shuffledQuestions', JSON.stringify(shuffledQuestions));
+var questions = JSON.parse(localStorage.getItem('Quiz-Questions')) || [];
+
+if (questions.length === 0) {
+    alert("Ask admin to add questions");
+    window.location.href = "index.html";
 } else {
-    var shuffledQuestions = JSON.parse(localStorage.getItem('shuffledQuestions'));
-}
-
-function setQuestion() {
-    document.querySelector("#checkbtn").classList.remove("hideit");
-    document.querySelector("#nextbtn").classList.add("hideit");
-
-    var qnum = document.querySelector("#qnum");
-    var questiondetail = document.querySelector("#question");
-    var option1 = document.querySelector("#qop1");
-    var option2 = document.querySelector("#qop2");
-    var option3 = document.querySelector("#qop3");
-    var option4 = document.querySelector("#qop4");
-
-    var op1 = document.querySelector("#option1");
-    var op2 = document.querySelector("#option2");
-    var op3 = document.querySelector("#option3");
-    var op4 = document.querySelector("#option4");
-
-    var qnow = shuffledQuestions[qstoexecute];
-    op1.value = qnow.qop1;
-    op2.value = qnow.qop2;
-    op3.value = qnow.qop3;
-    op4.value = qnow.qop4;
-
-    qnum.innerHTML = `Question ${qstoexecute + 1}:`; // Dynamic question number
-    questiondetail.innerHTML = qnow.question;
-    option1.innerHTML = `1. ${qnow.qop1}`;
-    option2.innerHTML = `2. ${qnow.qop2}`;
-    option3.innerHTML = `3. ${qnow.qop3}`;
-    option4.innerHTML = `4. ${qnow.qop4}`;
-
-    // Remove any existing background color classes for highlighting:
-    var allOptions = document.querySelectorAll("label");
-    for (var i = 0; i < allOptions.length; i++) {
-        allOptions[i].classList.remove("bggreen", "bgred");
+    if (!localStorage.getItem('shuffledQuestions')) {
+        var shuffledQuestions = shuffle([...questions]);
+        localStorage.setItem('shuffledQuestions', JSON.stringify(shuffledQuestions));
+    } else {
+        var shuffledQuestions = JSON.parse(localStorage.getItem('shuffledQuestions'));
     }
-}
 
-function checkAnswer() {
-    var qnow = shuffledQuestions[qstoexecute];
-    var selectedOption = document.querySelector('input[name="option"]:checked');
-    document.querySelector("#checkbtn").classList.add("hideit");
-    document.querySelector("#nextbtn").classList.remove("hideit");
+    var qlength = shuffledQuestions.length;
 
-    if (selectedOption) {
-        var selectedValue = selectedOption.value;
-        console.log(selectedValue);
-        console.log(qnow.ans)
-        if (selectedValue === qnow.ans) {
-            selectedOption.nextElementSibling.classList.add("bggreen"); // Highlight correct answer (green)
-            correctans++;
-        } else {
-            selectedOption.nextElementSibling.classList.add("bgred"); // Highlight incorrect answer (red)
-            document.querySelector('input[value="' + qnow.ans + '"]').nextElementSibling.classList.add("bggreen"); // Highlight correct answer
+    function setQuestion() {
+        document.querySelector("#checkbtn").classList.remove("hideit");
+        document.querySelector("#nextbtn").classList.add("hideit");
+
+        var qnum = document.querySelector("#qnum");
+        var questiondetail = document.querySelector("#question");
+        var option1 = document.querySelector("#qop1");
+        var option2 = document.querySelector("#qop2");
+        var option3 = document.querySelector("#qop3");
+        var option4 = document.querySelector("#qop4");
+
+        var op1 = document.querySelector("#option1");
+        var op2 = document.querySelector("#option2");
+        var op3 = document.querySelector("#option3");
+        var op4 = document.querySelector("#option4");
+
+        var qnow = shuffledQuestions[qstoexecute];
+        op1.value = qnow.qop1;
+        op2.value = qnow.qop2;
+        op3.value = qnow.qop3;
+        op4.value = qnow.qop4;
+
+        qnum.innerHTML = `Question ${qstoexecute + 1}:`;
+        questiondetail.innerHTML = qnow.question;
+        option1.innerHTML = `1. ${qnow.qop1}`;
+        option2.innerHTML = `2. ${qnow.qop2}`;
+        option3.innerHTML = `3. ${qnow.qop3}`;
+        option4.innerHTML = `4. ${qnow.qop4}`;
+
+        var allOptions = document.querySelectorAll("label");
+        for (var i = 0; i < allOptions.length; i++) {
+            allOptions[i].classList.remove("bggreen", "bgred");
         }
-    } else {
-        alert("Please select an answer.");
-    }
-}
-
-function nextQuestion() {
-    var allRadioInputs = document.querySelectorAll('input[type="radio"]');
-    for (var i = 0; i < allRadioInputs.length; i++) {
-        allRadioInputs[i].checked = false;
     }
 
-    qstoexecute++;
-    if (qstoexecute < qlength) {
-        setQuestion();
-    } else {
-        document.querySelector("#qpanel").classList.add("hideit");
-        var marksdiv = document.querySelector("#marks");
-        marksdiv.classList.remove("hideit");
-        var user = JSON.parse(localStorage.getItem("currentUser"))
-        var uname = toTitleCase(user.name);
-        var Percentage = (correctans * 100) / qlength;
+    function checkAnswer() {
+        var qnow = shuffledQuestions[qstoexecute];
+        var selectedOption = document.querySelector('input[name="option"]:checked');
+        document.querySelector("#checkbtn").classList.add("hideit");
+        document.querySelector("#nextbtn").classList.remove("hideit");
 
-        marksdiv.innerHTML = `<h1>Mr. ${uname}</h1><p>Your score is ${correctans} out of ${qlength}</p><p>Total Percentage is ${Percentage}%<br> <a href="index.html">Logout</a></p>`;
-
-        localStorage.removeItem('shuffledQuestions');
+        if (selectedOption) {
+            var selectedValue = selectedOption.value;
+            if (selectedValue === qnow.ans) {
+                selectedOption.nextElementSibling.classList.add("bggreen"); // Highlight correct answer (green)
+                correctans++;
+            } else {
+                selectedOption.nextElementSibling.classList.add("bgred"); // Highlight incorrect answer (red)
+                document.querySelector('input[value="' + qnow.ans + '"]').nextElementSibling.classList.add("bggreen"); // Highlight correct answer
+            }
+        } else {
+            alert("Please select an answer.");
+        }
     }
+
+    function nextQuestion() {
+        var allRadioInputs = document.querySelectorAll('input[type="radio"]');
+        for (var i = 0; i < allRadioInputs.length; i++) {
+            allRadioInputs[i].checked = false;
+        }
+
+        qstoexecute++;
+        if (qstoexecute < qlength) {
+            setQuestion();
+        } else {
+            document.querySelector("#qpanel").classList.add("hideit");
+            var marksdiv = document.querySelector("#marks");
+            marksdiv.classList.remove("hideit");
+            var user = JSON.parse(localStorage.getItem("currentUser"));
+            var uname = toTitleCase(user.name);
+            var Percentage = (correctans * 100) / qlength;
+
+            marksdiv.innerHTML = `<h1>Mr. ${uname}</h1><p>Your score is ${correctans} out of ${qlength}</p><p>Total Percentage is ${Percentage}%<br> <a href="index.html">Logout</a></p>`;
+
+            localStorage.removeItem('shuffledQuestions');
+        }
+    }
+
+    function toTitleCase(str) {
+        return str.replace(
+            /\w\S*/g,
+            text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+        );
+    }
+
+    setQuestion();
 }
-
-
-function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
-    );
-}
-
-setQuestion();
